32e8a60985243b80b32b534de6e8af3277832fb8
Elwa88 <fsociety.core@gmail.com>
2024-08-13 21:19:24 +0400

    set up Dockerfile and docker-compose/ minor routing adjustments

--- notes/urls.py
+++ notes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import NoteListCreate, NoteUpdateDestroy
 
 urlpatterns = [
-    path('notes/',NoteListCreate.as_view()),
-    path('notes/<int:pk>',NoteUpdateDestroy.as_view())
+    path('notes/', NoteListCreate.as_view()),
+    path('notes/<int:pk>', NoteUpdateDestroy.as_view())
 
 ]
